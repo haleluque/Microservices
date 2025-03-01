de107cac23c081ea4ccd7dbd9c3ef5534f9d1746
haleluque <henry-luque@hotmail.com>
2025-03-01 08:41:53 -0500

    Base microservice project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Microservice Spring Application
+
+Spring boot project with multiple microservices
+
+This is a fully built microservices Spring boot application example:
+- Spring boot version: 3.4.2
+- Java version: 21
+- Maven version: 3.9.6
+
+## Main Features
+
+### Limits Service
+Ports: 8080, 8081, etc.
+
+### spring cloud service
---
+++ limits-service/src/main/java/limits_service/LimitsServiceApplication.java
@@ -0,0 +1,12 @@
+package limits_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+}
